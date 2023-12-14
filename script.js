0888a422e93966ee51fcd9957529cd8f2d5c5e23
GowthamXeno <gowthamudpm2004@gmail.com>
2023-12-14 13:12:17 +0530

    Made Readable

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
+// JavaScript logic for calculator functionality
+
+// initializing display variable with element 
 var display = document.querySelector('.display');
 var currentInput = '0';
 
+// Function to update the display with the current input
 function updateDisplay(){
     const displayedText = currentInput.replace(/\*\*2/g, '²');
     display.textContent = displayedText;
 }
 
+
+// Function to clear the display
 function clearDisplay(){
     currentInput = '0';
     updateDisplay();
 }
 
+// Function to append a character to the current input
 function appendCharacter(char){ 
     if(currentInput === 'Error')
         currentInput ='';
@@ -21,6 +28,7 @@ function appendCharacter(char){
     updateDisplay();
 }
 
+// Function to perform the calculation
 function calculate(){
     try{
         currentInput = eval(currentInput).toString();
